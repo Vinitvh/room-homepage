9dd198a19da75b061ea14215e816941604e25823
Vinit Hemadri <vineet.vh@gmail.com>
2022-03-23 22:29:13 +0530

    Created layout using grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#A1A1A1",
+      secondary: "#454545",
+    },
   },
   plugins: [],
 };
